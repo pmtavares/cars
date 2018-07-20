642fb637536b0e1a1623762f477e0d9d9acc6ab7
Pedro <pedrotavares41@gmail.com>
2018-07-20 20:52:40 +0100

    Configured Postgresql connection; Able to insert and retrieve data from DB

--- cars/src/main/java/com/cars/models/Car.java
+++ cars/src/main/java/com/cars/models/Car.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Car {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 		
 	private String name;
